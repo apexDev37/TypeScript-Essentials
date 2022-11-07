ef3d871fcabd40bc8ecd0103faf55248705ea930
apexdev <mwangi.em37@gmail.com>
2022-11-07 18:17:48 +0300

    refactor: Add types and generics
    Add types to functions: addTodoItem and getNextId. Add generic to function: getNextId to enfore strong typing on functions.

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ interface Todo {
     id: number;
     title: string; 
     status: TodoStatus; 
-    completedOn: Date;
+    completedOn?: Date;
 }
 
 
@@ -15,21 +15,22 @@ enum TodoStatus {
 }
 
 
-const todoItems = [
-    {id: 1, title: "Learn HTML", status: "done", completedOn: new Date("2021-09-11")},    
-    { id: 1, title: "Learn HTML", status: "done", completedOn: new Date("2021-09-11") },
-    { id: 2, title: "Learn TypeScript", status: "in-progress" },
-    { id: 3, title: "Write the best app in the world", status: "todo" },
+const todoItems: Todo[] = [
+    { id: 1, title: "Learn HTML", status: TodoStatus.Done, completedOn: new Date("2021-09-11") },    
+    { id: 2, title: "Learn HTML", status: TodoStatus.InProgress },
+    { id: 3, title: "Learn TypeScript", status: TodoStatus.Done, completedOn:  new Date("2021-09-11") },
+    { id: 4, title: "Write the best app in the world", status: TodoStatus.Todo,  },
 ]
 
 
-function addTodoItem(todo) {
-    const id = getNextId(todoItems)
+// Instruction 03: Apply types to the parameters and return values for the functions
+function addTodoItem(todo: string): Todo {
+    const id = getNextId<Todo>(todoItems)
 
     const newTodo = {
-        id,
+        id: id,
         title: todo,
-        status: "todo",
+        status: TodoStatus.Todo,
     }
 
     todoItems.push(newTodo)
@@ -37,10 +38,14 @@ function addTodoItem(todo) {
     return newTodo
 }
 
-function getNextId(items) {
+
+// Instruction 04: Apply a generic parameter type to the below function
+function getNextId<T extends {id: number}>(items: T[]): number {
     return items.reduce((max, x) => x.id > max ? max : x.id, 0) + 1
 }
 
+
 const newTodo = addTodoItem("Buy lots of stuff with all the money we make from the app")
+
 
 console.log(JSON.stringify(newTodo))
